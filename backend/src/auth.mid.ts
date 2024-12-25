47f18913911539aedda715b903fcb4aa70a33fdf
Mufidat-Ahmed <imufidatahmad@gmail.com>
2024-12-25 13:18:32 +0100

    edit

--- backend/src/auth.mid.ts
+++ backend/src/auth.mid.ts
@@ -4,7 +4,7 @@ export default (req: any, res: any, next: any) => {
   const token = req.header.access_token as string;
   if (!token) return res.status(401).send('Access Denied');
 
-  try { const decodedUser = verify(token, process.env.JWT_SECRET!);
+  try { const decodedUser = verify(token, "secret");
     req.user = decodedUser;
 
   } catch (err) {
